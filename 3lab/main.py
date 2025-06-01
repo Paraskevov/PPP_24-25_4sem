2102ac3389acf8be1613c7f8ae1947f3da7423ad
Paraskevov <ivan.paraskevov2005@mail.ru>
2025-06-01 17:57:28 +0300

    Add files via upload

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,8 +1,15 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
 
-def main():
-    # Ваш код здесь
-    pass
+from app.api.routes import router as api_router
+from app.websocket.routes import router as ws_router
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
 
+app = FastAPI(title="Lab-3 Async API")
+
+app.include_router(api_router, prefix="/api")
+app.include_router(ws_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
